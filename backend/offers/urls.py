0495e9081e0c967f9d3a321a43682b25f3a6ab5c
RazvanTDF <razvantdf@gmail.com>
2025-05-18 15:06:39 +0300

    refactor: Remove offers app and related files

--- backend/offers/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import index
-from .api_views import OfferListView
-
-urlpatterns = [
-    path('', index, name='index'),  # returnează JSON cu toate ofertele + best
-    path('api/offers/', OfferListView.as_view(), name='offer-list'),  # API REST
-]
